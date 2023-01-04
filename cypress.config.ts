829256701385f0f807451b9b056ab72ecdb20fd1
Truong Tri Tin <eriegin@nexondv.com>
2023-01-04 15:35:40 +0700

    add swr revalidation test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 import { addBand } from "./lib/features/bands/queries";
+import { addReservation } from "./lib/features/reservations/queries";
 import { resetDB } from "./__tests__/__mocks__/db/utils/reset-db";
 
 export default defineConfig({
@@ -16,6 +17,8 @@ export default defineConfig({
       on("task", {
         "db:reset": () => resetDB().then(() => null),
         addBand: (newBand) => addBand(newBand).then(() => null),
+        addReservation: (newReservation) =>
+          addReservation(newReservation).then(() => null),
       });
     },
   },
