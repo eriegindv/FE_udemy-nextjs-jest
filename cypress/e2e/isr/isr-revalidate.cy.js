4aea67006e8bac54a854cd9455f570c04cc76a1f
Truong Tri Tin <eriegin@nexondv.com>
2023-01-04 14:41:00 +0700

    add testing for shows

--- cypress/e2e/isr/isr-revalidate.cy.js
+++ cypress/e2e/isr/isr-revalidate.cy.js
@@ -1,5 +1,6 @@
 import { generateRandomId } from "@/lib/features/reservations/utils";
 import { generateNewBand } from "@/__tests__/__mocks__/fakeData/newBand";
+import { generateNewShow } from "@/__tests__/__mocks__/fakeData/newShow";
 
 it("should load refreshed page from cache after new band is added", () => {
   // check that new band is not on page
@@ -26,5 +27,29 @@ it("should load refreshed page from cache after new band is added", () => {
     .should("exist");
 
   // reset ISR cache to initial db conditions
+  cy.resetDbAndIsrCache();
+});
+
+it("should load refreshed page from cache after new show is added", () => {
+  cy.task("db:reset").visit("/shows");
+  cy.get("h2")
+    .contains(/Avalanche of Cheese/i)
+    .should("not.exist");
+
+  const showId = generateRandomId();
+  const newShow = generateNewShow(showId);
+  const secret = Cypress.env("REVALIDATION_SECRET");
+
+  cy.request("POST", `api/shows?secret=${secret}`, { newShow }).then(
+    (response) => {
+      expect(response.body.revalidated).to.equal(true);
+    }
+  );
+
+  cy.reload();
+  cy.get("h2")
+    .contains(/Avalanche of Cheese/i)
+    .should("exist");
+
   cy.resetDbAndIsrCache();
 });
