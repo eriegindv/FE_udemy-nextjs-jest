5c5f36ed72d202b804d24b531709df678f9dfe8b
Truong Tri Tin <eriegin@nexondv.com>
2023-01-09 11:01:24 +0700

    test route with url param

--- __tests__/api/shows.test.ts
+++ __tests__/api/shows.test.ts
@@ -1,14 +1,15 @@
 import { expect } from "@jest/globals";
 import { testApiHandler } from "next-test-api-route-handler";
 
+import showIdHandler from "@/pages/api/shows/[showId]";
 // Import the handler under test from the pages/api directory
-import handler from "@/pages/api/shows/index";
+import showsHandler from "@/pages/api/shows/index";
 
 import { readFakeData } from "../__mocks__/fakeData";
 
-it("/api/shows returns shows from database", async () => {
+it("GET /api/shows returns shows from database", async () => {
   await testApiHandler({
-    handler,
+    handler: showsHandler,
     test: async ({ fetch }) => {
       const res = await fetch({ method: "GET" });
       expect(res.status).toBe(200);
@@ -17,6 +18,25 @@ it("/api/shows returns shows from database", async () => {
       const { fakeShows } = await readFakeData();
 
       expect(json).toEqual({ shows: fakeShows });
+    },
+  });
+});
+
+it("GET /api/shows/[showId] returns the data for the correct show ID", async () => {
+  await testApiHandler({
+    handler: showIdHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.showId = 0;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(200);
+
+      const json = await res.json();
+      const { fakeShows } = await readFakeData();
+
+      expect(json).toEqual({ show: fakeShows[0] });
     },
   });
 });
