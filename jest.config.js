82b40af51067715b2d8fc89e3630affeb6f95fb3
Truong Tri Tin <eriegin@nexondv.com>
2023-01-09 13:53:42 +0700

    prevent parallelization in testing

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,7 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   // No restart test when files are changed
   watchPathIgnorePatterns: ["<rootDir>/__tests__/__mocks__/db/.*\\.json"],
+  maxWorkers: 1,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
